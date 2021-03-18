4e8e27393d70a17c1bc3690f0ff833c0464b1c14
vkgtaro <vkg.taro@gmail.com>
2021-03-19 00:12:33 +0900

    fixed to read corect module.

--- test/echonet-lite/property-parser/02-88-E0.test.js
+++ test/echonet-lite/property-parser/02-88-E0.test.js
@@ -1,8 +1,8 @@
-const parser = require('../../../src/echonet-lite/property-parser/02-88-E7.js')
+const parser = require('../../../src/echonet-lite/property-parser/02-88-E0.js')
 
 test('parse', () => {
   expect(parser(Buffer.from('0001D405', 'hex'))).toBe(119813)
   expect(parser(Buffer.from('00000000', 'hex'))).toBe(0)
-  expect(parser(Buffer.from('05F5E0FF', 'hex'))).toBe(99999o999)
+  expect(parser(Buffer.from('05F5E0FF', 'hex'))).toBe(99999999)
 })
 
