3b64046614f145bcf234c730ff0d0d0e9af53db9
vkgtaro <vkg.taro@gmail.com>
2021-03-21 01:24:44 +0900

    added 02-88-E2

---
+++ src/echonet-lite/property-parser/02-88-E2.js
@@ -0,0 +1,14 @@
+const parser = (buf) => {
+  const result = []
+  result.push(buf.slice(0, 2).readUInt16BE())
+
+  let offset = 2
+  for (let i = 0; i < 48; i++) {
+    const total = buf.slice(offset, offset+4)
+    result.push(total.readUInt32BE())
+    offset += 4
+  }
+
+  return result
+}
+module.exports = parser
