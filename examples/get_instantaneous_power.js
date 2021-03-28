a38302efb64707236949b3c18064cc5113d8bbc4
vkgtaro <vkg.taro@gmail.com>
2021-03-29 01:47:06 +0900

    fixed ESlint

--- examples/get_instantaneous_power.js
+++ examples/get_instantaneous_power.js
@@ -1,58 +1,55 @@
-const RL7023StickDIPS = require('./src/rl7023-stick-d-ips')
-const EchonetLiteRequest = require('./src/echonet-lite/request')
-const EchonetLiteResponse = require('./src/echonet-lite/response')
-const retry = require('./src/simple-retry')
-const FileCache = require('./src/file-cache')
+const RL7023StickDIPS = require('../src/rl7023-stick-d-ips')
+const retry = require('../src/simple-retry')
+const FileCache = require('../src/file-cache')
 const Influx = require('influx')
 
 // Bルート認証ID
-const broute_id = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+const BROUTE_ID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 // Bルート認証パスワード
-const broute_password = 'XXXXXXXXXXXX'
+const BROUTE_PASSWORD = 'XXXXXXXXXXXX'
 // RL7023 Stick-D/IPS USB デバイス
-const device_path = '/dev/ttyUSB_power'
+const DEVICE_PATH = '/dev/ttyUSB_power'
 
 // influx database name
-const influx_db_name = 'db_name'
+const INFLUX_DB_NAME = 'db_name'
 
 const influx = new Influx.InfluxDB({
   host: 'localhost',
-  database: influx_db_name,
+  database: INFLUX_DB_NAME
 })
 
-const insert_data = (measurement, time=new Date(), fields, tags) => {
+const insertData = (measurement, time = new Date(), fields, tags) => {
   influx.writePoints([{
-    'measurement': measurement,
-    'tags': tags,
-    'fields': fields,
-    'timestamp': time,
+    measurement: measurement,
+    tags: tags,
+    fields: fields,
+    timestamp: time
   }], {
     precision: Influx.Precision.Milliseconds
   })
-  .catch(error => {
-    console.error(`Error saving data to InfluxDB! ${error.stack}`)
-  });
+    .catch(error => {
+      console.error(`Error saving data to InfluxDB! ${error.stack}`)
+    })
 }
 
-const get_now = () => {
+const getNow = () => {
   const date = new Date()
   date.setMilliseconds(0)
   date.setSeconds(0)
   return date
 }
 
-const rl7023 = new RL7023StickDIPS(device_path)
+const rl7023 = new RL7023StickDIPS(DEVICE_PATH)
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
   rl7023.close()
   process.exit(1)
 })
 
-
 const main = async (now, rl7023) => {
   try {
-    await rl7023.sksetrbid(broute_id)
-    await rl7023.sksetpwd(broute_password)
+    await rl7023.sksetrbid(BROUTE_ID)
+    await rl7023.sksetpwd(BROUTE_PASSWORD)
 
     const cache = new FileCache('smart-meter-device-cache')
     let device = cache.get('device')
@@ -60,22 +57,22 @@ const main = async (now, rl7023) => {
       device = await retry(async () => {
         return await rl7023.skscan()
       }, 6)
-      device.ipv6_addr = await rl7023.skll64(device['Addr'])
+      device.ipv6Addr = await rl7023.skll64(device.Addr)
       cache.set('device', device)
     }
     console.log(device)
-    await rl7023.sksreg_channel(device['Channel'])
-    await rl7023.sksreg_pan_id(device['Pan ID'])
-    rl7023.set_ipv6_addr(device.ipv6_addr)
+    await rl7023.sksregChannel(device.Channel)
+    await rl7023.sksregPanId(device['Pan ID'])
+    rl7023.setIPv6Addr(device.ipv6Addr)
     await rl7023.skjoin()
 
     const res = await retry(async () => {
-      return await rl7023.request_echonet_lite('Get', [{epc: 0xE7}])
+      return await rl7023.requestEchonetLite('Get', [{ epc: 0xE7 }])
     }, 2)
-    const instantaneous_power = res.get_parsed_properties()[0] / 1000
-    console.log(instantaneous_power + 'kw')
+    const instantaneousPower = res.getParsedProperties()[0] / 1000
+    console.log(instantaneousPower + 'kw')
 
-    insert_data('power', now, {'instantaneous_power': instantaneous_power}, {type: 'electricity_meter'})
+    insertData('power', now, { instantaneousPower: instantaneousPower }, { type: 'electricity_meter' })
     rl7023.close()
   } catch (error) {
     console.log(error)
@@ -84,5 +81,5 @@ const main = async (now, rl7023) => {
   }
 }
 
-const now = get_now()
+const now = getNow()
 main(now, rl7023)
