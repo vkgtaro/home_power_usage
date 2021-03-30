121b0a18ca4b4e478e33ee472be21fc005793976
vkgtaro <vkg.taro@gmail.com>
2021-03-31 00:25:49 +0900

    added 02-79-E3

---
+++ test/echonet-lite/property-parser/02-79-E3.test.js
@@ -0,0 +1,6 @@
+const parser = require('../../../src/echonet-lite/property-parser/02-79-E3.js')
+
+test('parse', () => {
+  expect(parser(Buffer.from('00000000', 'hex'))).toBe(0)
+  expect(parser(Buffer.from('3B9AC9FF', 'hex'))).toBe(999999.999)
+})
